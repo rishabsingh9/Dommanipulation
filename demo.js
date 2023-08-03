247d8e62fc53b5d4ab8abe79a3a27732e25d5de3
rishabsingh9 <rishabsinghrs1733@gmail.com>
2023-08-03 18:55:38 +0530

    filter added

--- demo.js
+++ demo.js
@@ -1,10 +1,16 @@
 var form=document.getElementById('addForm')
 var itemlist=document.getElementById('items')
+var decriptionlist=document.getElementById('description')
+var filter=document.getElementById('filter')
+
 form.addEventListener('submit',additem)
 itemlist.addEventListener('click',remove)
+filter.addEventListener('keyup',filteritems)
 function additem(e){
     e.preventDefault();
     var inputitem=document.getElementById('item').value;
+    var descriptionitem=document.getElementById('description').value
+    inputitem=inputitem+" "+descriptionitem
     var li=document.createElement('li')
     li.className='list-group-item'
     li.appendChild(document.createTextNode(inputitem));
@@ -30,4 +36,25 @@ function remove(e){
             itemlist.removeChild(li);
         }
     }
+}
+
+function filteritems(e){
+    //converting text to lowercase
+    var text=e.target.value.toLowerCase()
+    
+    var items=itemlist.getElementsByTagName('li')
+    Array.from(items).forEach(function(item){
+        var itemname=item.firstChild.textContent
+        var second=item.childNodes
+        var d=second[1].textContent
+
+
+        console.log(d)
+        if(itemname.toLowerCase().indexOf(text)!=-1 || d.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    });
 }
